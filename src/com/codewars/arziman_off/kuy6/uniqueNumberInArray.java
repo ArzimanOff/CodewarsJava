a54e9cc206295db16375a782395b80753dd3013b
ArzzzimanOff <arzimanovgyulahmed@yandex.ru>
2023-10-23 13:01:45 +0300

    new Tasks

--- src/com/codewars/arziman_off/kuy6/uniqueNumberInArray.java
+++ src/com/codewars/arziman_off/kuy6/uniqueNumberInArray.java
@@ -20,16 +20,7 @@ public class uniqueNumberInArray {
     }
 
     public static void main(String[] args) {
-        double[] arr = new double[1000000];
-        for (int i = 1; i < 1000000; i++) {
-            if (i == 734534){
-                arr[i] = 55556;
-            } else {
-                arr[i] = 55555;
-            }
-        }
         System.out.println(findUniq(new double[]{1, 1, 1, 2, 1, 1})); // Output: 2.0
-        System.out.println(findUniq(arr));
         System.out.println(findUniq(new double[]{0, 0, 0.55, 0, 0}));   // Output: 0.55
     }
 }
