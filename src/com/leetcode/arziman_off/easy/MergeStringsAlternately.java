2fe46718855fb1773ab36ce6324e8532127e8585
ArzzzimanOff <arzimanovgyulahmed@yandex.ru>
2024-02-22 00:49:02 +0300

    очередная простенькая задачка

---
+++ src/com/leetcode/arziman_off/easy/MergeStringsAlternately.java
@@ -0,0 +1,21 @@
+package com.leetcode.arziman_off.easy;
+
+public class MergeStringsAlternately {
+    public static void main(String[] args) {
+        System.out.println(mergeAlternately("abc", "defg"));
+    }
+    public static String mergeAlternately(String word1, String word2) {
+        StringBuilder ans = new StringBuilder();
+        int i = 0;
+        while(i < word1.length() || i < word2.length()){
+            if (i < word1.length()){
+                ans.append(word1.charAt(i));
+            }
+            if (i < word2.length()){
+                ans.append(word2.charAt(i));
+            }
+            i++;
+        }
+        return ans.toString();
+    }
+}
