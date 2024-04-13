2f134aed1ef5fdd8de1cd26de57e3096467ae20a
ArzzzimanOff <arzimanovgyulahmed@yandex.ru>
2024-04-13 22:15:36 +0300

    Реализована быстрая сортировка

--- src/com/other_tasks/QuickSort.java
+++ src/com/other_tasks/QuickSort.java
@@ -16,6 +16,37 @@ public class QuickSort {
     }
 
     private static void quickSort(int[] arr) {
+        if (arr.length != 0){
+            quickSortFunc(arr, 0, arr.length - 1);
+        }
+    }
+    public static void quickSortFunc(int[] arr, int left, int right) {
+        int i = left;
+        int j = right;
+        int pivotIndex = i + (j - i) / 2;
+        int pivot = arr[pivotIndex];
+        while (i <= j){
+            while (arr[i] < pivot){
+                i++;
+            }
+            while (arr[j] > pivot){
+                j--;
+            }
+            if (i <= j){
+                swapArrItems(arr, i++, j--);
+            }
+        }
+        if (left < j) {
+            quickSortFunc(arr, left, j);
+        }
+        if (i < right) {
+            quickSortFunc(arr, i, right);
+        }
+    }
 
+    public static void swapArrItems(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
